488d37feb996197875075823f9d9f3c628756bb8
Gabriel5489 <vegetaevil7@gmail.com>
2021-11-29 04:10:21 -0600

    Modal de productos

--- src/navigation/AppNavigation.js
+++ src/navigation/AppNavigation.js
@@ -1,6 +1,7 @@
-import React from 'react'
-import { IconButton } from 'react-native-paper';
+import React, {useState} from 'react'
+import { IconButton, Modal, Text, Portal, Provider, View, Checkbox, Card, Avatar, Button } from 'react-native-paper';
 import { createDrawerNavigator } from '@react-navigation/drawer'
+import {useFormik} from 'formik';
 import { NavigationContainer } from '@react-navigation/native';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import Search from '../componentes/Search';
@@ -12,10 +13,84 @@ import CustomDrawerContent from "./CustomDrawerContent"
 import AddressStack from './AddressStack';
 import AccountStack from './AccountStack'
 import colors from '../style/colors';
+import SearchAvanzada from '../componentes/Search/SearchAvanzada';
+import ProductList from '../componentes/Search/ProductList';
+
+import {createStackNavigator} from "@react-navigation/stack";
+import { useNavigation, } from '@react-navigation/native';
+import { advSearch } from '../api/Search';
+
+
 const Drawer = createDrawerNavigator();
 
-export default function AppNavigation() {
+export default function AppNavigation(navigation) {
+    const [visible, setVisible] = useState(false);
+    const [checked, setChecked] = useState(false);
+    const [checked2, setChecked2] = useState(false);
+    const [checked3, setChecked3] = useState(false);
+    const [checked4, setChecked4] = useState(false);
+    const [loading, setloading] = useState(false);
+    const showModal = () => setVisible(true);
+    const hideModal = () => setVisible(false);
+    const containerStyle =  { backgroundColor:'white', padding: 20};
+
+    const formik = useFormik({
+        initialValues: initialValues(),
+        onSubmit: async (formData) => {
+            hideModal();
+            try{
+                const response = await advSearch(formData);
+                
+            }catch (error){
+                console.log(error);
+            }
+        }
+    });
+
     return (
+        <>
+            <Portal>
+                <Modal 
+                visible={visible} onDismiss={hideModal} contentContainerStyle={containerStyle} >
+                    <Card.Title title="Busqueda avanzada" />
+                        <Checkbox.Item
+                        label="Extra Chica" 
+                        status={checked ? 'checked' : 'unchecked'}
+                        onPress={() => {
+                          setChecked(!checked);
+                          checked ? formik.setFieldValue("extra_chica", "") : formik.setFieldValue("extra_chica", "extra chica");
+                        }} />
+
+                        <Checkbox.Item
+                        label="Chica" 
+                        status={checked2 ? 'checked' : 'unchecked'}
+                        onPress={() => {
+                          setChecked2(!checked2);
+                          checked2 ? formik.setFieldValue("chica", "") : formik.setFieldValue("chica", "chica");
+                        }} />
+
+                        <Checkbox.Item
+                        label="Mediana" 
+                        status={checked3 ? 'checked' : 'unchecked'}
+                        onPress={() => {
+                          setChecked3(!checked3);
+                          checked3 ? formik.setFieldValue("mediana", "") : formik.setFieldValue("mediana", "mediana");
+                        }} />
+
+                        <Checkbox.Item
+                        label="Grande" 
+                        status={checked4 ? 'checked' : 'unchecked'}
+                        onPress={() => {
+                          setChecked4(!checked4);
+                          checked4 ? formik.setFieldValue("grande", "") : formik.setFieldValue("grande", "grande");
+                        }} />
+                    <Button
+                    icon="filter"
+                    mode="contained"
+                    style={{marginTop: 30}}
+                    onPress={ formik.handleSubmit } >Buscar</Button>
+                </Modal>
+            </Portal>
         <NavigationContainer>
             <Drawer.Navigator initialRouteName="Home"
                 drawerStyle={{
@@ -31,7 +106,7 @@ export default function AppNavigation() {
                         <Search />),
                     drawerIcon: ({ color }) => (
                         <MaterialCommunityIcons name="home" color={color} size={26} />
-                    ), headerShown: true, headerRight: () => (<IconButton icon="dots-vertical" onPress={() => { }} />),
+                    ), headerShown: true, headerRight: () => (<IconButton icon="dots-vertical" onPress={ () => { showModal() }} />),
                     gestureEnabled: true,
                     ///headerLeft: () => (<Button onPress={() => { console.log("Press") }} ><MaterialCommunityIcons name="home" color={colors.success} size={40} /> </Button>)
 
@@ -82,7 +157,18 @@ export default function AppNavigation() {
                 }}
                 />
 
+
             </Drawer.Navigator>
         </NavigationContainer>
+     </>   
     )
 }
+
+function initialValues(){
+    return{
+        extra_chica:"",
+        chica:"",
+        mediana:"",
+        grande:""
+    }
+};
